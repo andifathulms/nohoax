5b0b58706ebe06b5c905427955801fd83e0d9ce2
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-09-06 02:53:48 +0700

    cover temp pass

--- nohoax/settings.py
+++ nohoax/settings.py
@@ -146,5 +146,5 @@ AUTH_USER_MODEL = 'users.user'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rubyghozal@gmail.com'
-EMAIL_HOST_PASSWORD = 'sapibetina'
+EMAIL_HOST_PASSWORD = '###'
 EMAIL_PORT = 587
