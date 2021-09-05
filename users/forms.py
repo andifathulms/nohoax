2bd2fd94ea14000aeafc6be537ae795da7443ad0
Andi Fathul <35810127+andifathulms@users.noreply.github.com>
2021-09-05 12:12:51 +0700

    Complete registration constraint

--- users/forms.py
+++ users/forms.py
@@ -21,11 +21,20 @@ class UserRegisterForm(forms.ModelForm):
 		model = user
 		fields = ['name','email','nohp','password','confirm_password']
 	def clean(self):
-	    cleaned_data = super(UserRegisterForm, self).clean()
-	    password = cleaned_data.get("password")
-	    confirm_password = cleaned_data.get("confirm_password")
+		cleaned_data = super(UserRegisterForm, self).clean()
+		password = cleaned_data.get("password")
+		confirm_password = cleaned_data.get("confirm_password")
 
-	    if password != confirm_password:
-	        self.add_error('confirm_password', "Password does not match")
+		if password != confirm_password:
+			self.add_error('confirm_password', "Password does not match")
 
-	    return cleaned_data
+		return cleaned_data
+	
+	def clean_email(self):
+		cleaned_data = super(UserRegisterForm, self).clean()
+		email = cleaned_data.get('email')
+		try:
+			match = user.objects.get(email = email)
+			self.add_error('email', "This email address is already in use")
+		except:
+			return email
